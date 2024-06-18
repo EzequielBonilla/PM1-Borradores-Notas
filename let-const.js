c46ae54c91d6a0311f661dedce3b777a80f129a7
EzequielBonilla <ezequiel.bonilla92@gmail.com>
2024-06-18 00:49:59 -0300

    notas

---
+++ let-const.js
@@ -0,0 +1,33 @@
+// var a=5;
+// var b=10;
+
+// var suma = a + b;
+
+// console.log(suma);
+
+
+// let a = 5;
+
+// a=15;
+// let b = 8;
+// let suma = a+b;
+
+// console.log(suma);
+
+// const a = 5;
+
+// const b=7;
+
+// const suma=a+b;
+
+// console.log(suma);
+// //const se utiliza para declarar variables que no pueden mutar, que sen "constantes"
+
+
+var a="declarada con var";
+var a="redeclarar variable";
+console.log(a);
+
+
+let b="declarada con let";
+const c="declarada con const";
